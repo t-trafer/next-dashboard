d9afbd04d7baf82f12a079df7be1627a3e7bd500
Nilesh <nilesh.jha@onequince.com>
2024-05-30 03:16:36 +0530

    initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+console.log('root out');
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  console.log('root in');
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
